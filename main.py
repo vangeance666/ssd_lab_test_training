90aaf160e85da93f8fe4fd95cc068e3ad32d3d3e
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2021-12-02 03:07:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from waitress import serve
 
 if __name__ == '__main__':
 	app = create_app()
-    serve(app, host="0.0.0.0", port=5000) # nosec
+	serve(app, host="0.0.0.0", port=5000)
 	# app.run(debug=False, host='0.0.0.0')
