acfa1b47df056583f06815aab0bf56872fbed1be
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2021-12-02 16:55:31 +0800

    f

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, request, session, render_template, redirect, url_for
 
 def create_app():
     app = Flask(__name__)
-
+    app.config['SECRET_KEY'] = 'the random string' 
     from website.views.main_view import main_view
     app.register_blueprint(main_view, url_prefix='/')
 
