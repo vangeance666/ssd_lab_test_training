83d75861b019d58d555cbdb4adb68bf7a0808576
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2021-12-02 17:06:51 +0800

    c

--- website/__init__.py
+++ website/__init__.py
@@ -2,9 +2,15 @@ import os
 from flask import Flask, request, session, render_template, redirect, url_for
 
 
+
+from flask_wtf.csrf import CSRFProtect, CSRFError
+
+csrf = CSRFProtect()
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'the random string' 
+    csrf.init_app(app)
     from website.views.main_view import main_view
     app.register_blueprint(main_view, url_prefix='/')
 
